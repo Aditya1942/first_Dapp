1c147c3dc691420757bdaadc07d1f4e278086547
Aditya1942 <parmaraditya1942@gmail.com>
2022-02-02 19:53:13 +0530

    completed

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import getWeb3 from "./getWeb3";
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { storageValue: 0, web3: null, accounts: null, contract: null, value: 0 };
 
   componentDidMount = async () => {
     try {
@@ -20,7 +20,7 @@ class App extends Component {
       const deployedNetwork = SimpleStorageContract.networks[networkId];
       const instance = new web3.eth.Contract(
         SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
+        deployedNetwork && deployedNetwork.address
       );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
@@ -29,26 +29,15 @@ class App extends Component {
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
+        `Failed to load web3, accounts, or contract. Check console for details.`
       );
       console.error(error);
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
-  setStorageValue = async (e) => {
-    let value = parseInt(e);
+  setStorageValue = async () => {
+    let value = parseInt(this.state.value)
     if (typeof value !== "number") {
       alert("invalid value");
     } else {
@@ -67,22 +56,121 @@ class App extends Component {
     // Update state with the result.
     console.log(response);
     this.setState({ storageValue: response });
-  }
-
+  };
 
   render() {
     if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
+      return (
+        <div className="bg-white w-full h-screen flex justify-center items-center">
+          Loading Web3, accounts, and contract...
+        </div>
+      );
     }
     return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <h2>Simple Contract Example for storeing value in blockchain</h2>
- 
-
-        <div>The stored value is: {this.state.storageValue}</div>
-
-        
+      <div className="bg-white py-16 sm:py-24">
+        <div className="relative sm:py-16">
+          <div aria-hidden="true" className="hidden sm:block">
+            <div className="absolute inset-y-0 left-0 w-1/2 bg-gray-50 rounded-r-3xl" />
+            <svg
+              className="absolute top-8 left-1/2 -ml-3"
+              width={404}
+              height={392}
+              fill="none"
+              viewBox="0 0 404 392"
+            >
+              <defs>
+                <pattern
+                  id="8228f071-bcee-4ec8-905a-2a059a2cc4fb"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-gray-200"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width={404}
+                height={392}
+                fill="url(#8228f071-bcee-4ec8-905a-2a059a2cc4fb)"
+              />
+            </svg>
+          </div>
+          <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+            <div className="relative rounded-2xl px-6 py-10 bg-indigo-600 overflow-hidden shadow-xl sm:px-12 sm:py-20">
+              <div
+                aria-hidden="true"
+                className="absolute inset-0 -mt-72 sm:-mt-32 md:mt-0"
+              >
+                <svg
+                  className="absolute inset-0 h-full w-full"
+                  preserveAspectRatio="xMidYMid slice"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 1463 360"
+                >
+                  <path
+                    className="text-indigo-500 text-opacity-40"
+                    fill="currentColor"
+                    d="M-82.673 72l1761.849 472.086-134.327 501.315-1761.85-472.086z"
+                  />
+                  <path
+                    className="text-indigo-700 text-opacity-40"
+                    fill="currentColor"
+                    d="M-217.088 544.086L1544.761 72l134.327 501.316-1761.849 472.086z"
+                  />
+                </svg>
+              </div>
+              <div className="relative">
+                <div className="sm:text-center">
+                  <h2 className="text-3xl font-extrabold text-white tracking-tight sm:text-4xl">
+                    Good to Go!
+                  </h2>
+                  <p className="mt-6 mx-auto max-w-2xl text-lg text-indigo-200">Simple Ethereum Contract that stores value in blockchain
+                  </p>
+                </div>
+                <form
+                  action="#"
+                  className="mt-12 sm:mx-auto sm:max-w-lg sm:flex"
+                  onSubmit={this.setStorageValue}
+                >
+                  <div className="min-w-0 flex-1">
+                    <label htmlFor="cta-email" className="sr-only">
+                      Add Value
+                    </label>
+                    <input
+                      onChange={(e) => this.setState({ value: e.target.value })}
+                      type="number"
+                      className="block w-full border border-transparent rounded-md px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600"
+                      placeholder="Enter new value"
+                    />
+                  </div>
+                  <div className="mt-4 sm:mt-0 sm:ml-3">
+                    <button
+                      type="submit"
+                      className="block w-full rounded-md border border-transparent px-5 py-3 bg-indigo-500 text-base font-medium text-white shadow hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600 sm:px-10"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </form>
+                <div className="sm:text-center mt-10">
+                  <h2 className="text-3xl font-extrabold text-white tracking-tight sm:text-4xl">
+                    The stored value is: {this.state.storageValue}
+                  </h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
